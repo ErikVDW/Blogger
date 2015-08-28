dd00484e09a1b81324373409b6f9172b2393034a
erikvdw <ev176@georgetown.edu>
2015-08-28 23:13:49 +0000

    Finish I3

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,6 +3,6 @@ module ArticlesHelper
     @article = Article.find(params[:id])
   end
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :tag_list)
   end
 end
