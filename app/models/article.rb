328b2f147c6c843e1d6ce8ea4028c11c23f5dbff
erikvdw <ev176@georgetown.edu>
2016-02-02 22:24:42 +0000

    Edit articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+    has_attached_file :image, styles: {medium: "300x300>", thumb: "100x100>"}
+    validates_attachment_content_type :image, :content_type => ["image/jpeg", "image/png"]
     def tag_list=(tag_string)
       tag_names = tag_string.split(",").collect{|s| s.strip.downcase}.uniq
       new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
